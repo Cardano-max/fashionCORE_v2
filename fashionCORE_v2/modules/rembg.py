59b122a7dddb793821e8eaeb072f3e0002906540
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-06-24 13:05:01 +0500

    Move files to root of repository

--- fashionCORE_v2/modules/rembg.py
+++
@@ -1,8 +0,0 @@
-from rembg import remove
-import gradio as gr
-from PIL import Image
-
-def rembg_run(path, progress=gr.Progress(track_tqdm=True)):
-    input = Image.open(path)
-    output = remove(input)
-    return output
